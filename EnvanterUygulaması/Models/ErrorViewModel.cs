15d0eccff6f309a21f2958aa10905419a3d6f16a
Sena Ünalmış <81318091+senaunalmis@users.noreply.github.com>
2023-08-25 15:23:58 +0300

    update

--- EnvanterUygulaması/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EnvanterUygulaması.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
